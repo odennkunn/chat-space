88ab6486f484061e3d4d80bd82e598f35a8ba4fb
yoshitaka taniguchi <truck_mos1500@yahoo.co.jp>
2020-01-18 11:03:41 +0900

    アップローダー作成

---
+++ app/uploaders/avatar_uploader.rb
@@ -0,0 +1,17 @@
+class AvatarUploader < CarrierWave::Uploader::Base
+
+  include CarrierWave::MiniMagick
+
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+end
